c5b264a9c904fa9a182c8cf425ff79e59b01deb8
jannethsm31 <1722110301@utectulancingo.edu.mx>
2023-12-06 21:47:02 -0600

    FOR LOGGIN

---
+++ contactos.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    username VARCHAR(100) NOT NULL PRIMARY KEY,
+    password VARCHAR(50) NOT NULL,
+    token VARCHAR(130) NOT NULL,
+    timestamps TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO usuarios (username, password, token) VALUES ('hola@example.com', 'd6e2dbac6191d56db007ca489fb83cff', '7d8d45fc32e6865adb9959ae033c0350');
+
+INSERT INTO usuarios (username, password, token) VALUES ('jjj@example.com', '550167f3ca4205dca4d44a27dc24a3b6', '5c2e3a104b635a8d342f11176ecb3b2a');
+
+ALTER TABLE usuarios
+ADD COLUMN expiration_time INTEGER;
--- main.py
+++ main.py
@@ -1,64 +1,98 @@
-from fastapi.security import HTTPBearer
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPBasic, HTTPBasicCredentials
 from fastapi.security import HTTPAuthorizationCredentials
-from fastapi import Depends
+from fastapi.middleware.cors import CORSMiddleware
 import hashlib
-from fastapi.security import HTTPBasic
-from fastapi.security import HTTPBasicCredentials
 import sqlite3
+import threading
+import time
+import secrets
+from datetime import datetime, timedelta
 
-security = HTTPBasci()
-
-# Recupera el token
-securityBearer = HTTPBearer()
+security_basic = HTTPBasic()
+security_bearer = HTTPBearer()
 
 app = FastAPI()
 
-conn = sqlite3.connect('sql/usuarios.db')
+# Utiliza un pool de conexiones para manejar conexiones separadas por hilo
+conn_pool = threading.local()
 
 origins = [
-    "http://localhost:8080"
-    "http://172.0.0.0:8080"
+    "http://localhost:8080",
+    "http://172.0.0.0:8080",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
-@app.get('/')
-def root(credentials: HTTPAuthorizationCredentials = Depends(securityBearer)):
-    token = credentialsv.credentials
-    valor = ''
-    print(token)
-    c = conn.cursor()
 
-    c.execute("SELECT token FROM usuarios WHERE token = ?", (token,))
-    response = []
-    for fila in c:
-        validator = fila[0]
-        response.append(validator)
-    if not response:
+def get_conn():
+    # Crea una nueva conexión si no existe en el hilo actual
+    if not hasattr(conn_pool, 'conn'):
+        conn_pool.conn = sqlite3.connect('contactos.db')
+    return conn_pool.conn
+
+def create_token():
+    return secrets.token_urlsafe(16)
+
+def update_token(email):
+    new_token = create_token()
+    expiration_time = int(time.time()) + 600  # 10 minutos de duración
+    c = get_conn().cursor()
+    c.execute("UPDATE usuarios SET token = ?, expiration_time = ? WHERE username = ?", (new_token, expiration_time, email))
+    get_conn().commit()
+    return new_token, expiration_time
+
+@app.get('/')
+def root(credentials: HTTPAuthorizationCredentials = Depends(security_bearer)):
+    token = credentials.credentials
+    print(token)
+    c = get_conn().cursor()
+
+    c.execute("SELECT token, expiration_time FROM usuarios WHERE token = ?", (token,))
+    response = c.fetchone()
+    if not response or (response and response[1] is not None and response[1] < int(time.time())):
         return {"message": "Acceso denegado"}
     return {"message": "Acceso permitido"}
-    if token = "12treS":
-        return{"auth": True}
-    else
-        return{"auth": False}
 
+# Cada que el usuario se loggea, cambia el token
 @app.get("/token/")
-async def validate_user(credentials: HTTPBasicCredentials = Depends(security));
-    email = credentials .username
-    password_b = haslib.md5(credentials.password.encode())
-    password = password_b.hexdigest()
-    print(password)
-    c = conn.cursor()
-    c.execute("SELECT token FROM usuarios WHERE username = ? AND password = ?", (email, password))
-    response = []
-    for fila in c:
-        contacto = {"Token": fila[0]}
-        response.append(contacto)
+async def validate_user(credentials: HTTPBasicCredentials = Depends(security_basic)):
+    email = credentials.username
+    password_hash = hashlib.md5(credentials.password.encode()).hexdigest()
+    c = get_conn().cursor()
+    c.execute("SELECT token, expiration_time FROM usuarios WHERE username = ? AND password = ?", (email, password_hash))
+    response = c.fetchone()
     if not response:
-        response = []
-    return response
+        raise HTTPException(status_code=401, detail="Credenciales inválidas")
+
+    new_token, expiration_time = update_token(email)
+
+    return {"token": new_token, "expiration_time": expiration_time}
+
+
+'''
+# por tiempo (600s)
+@app.get("/token/")
+async def validate_user(credentials: HTTPBasicCredentials = Depends(security_basic)):
+    email = credentials.username
+    password_hash = hashlib.md5(credentials.password.encode()).hexdigest()
+    c = get_conn().cursor()
+    c.execute("SELECT token, expiration_time FROM usuarios WHERE username = ? AND password = ?", (email, password_hash))
+    response = c.fetchone()
+    if not response:
+        raise HTTPException(status_code=401, detail="Credenciales inválidas")
+
+    current_time = datetime.utcnow()  # Obtiene la hora actual en formato UTC
+
+    # Si el token ha expirado, actualiza el token y su expiración
+    if response and response[1] is not None and response[1] < int(current_time.timestamp()):
+        new_token, expiration_time = update_token(email)
+        return {"token": new_token, "expiration_time": expiration_time}
+
+    return {"token": response[0], "expiration_time": response[1]}
+'''
